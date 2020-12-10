4315a816e85c6609086cee08ea427916aa36c454
Nishant <nishant2106>
2020-12-10 23:07:54 +0530

    Report

--- bk.sql
+++ bk.sql
@@ -34,7 +34,7 @@ CREATE TABLE `Donation` (
   KEY `fk_nurse_id` (`nurse_id`),
   CONSTRAINT `fk_aadhar_no` FOREIGN KEY (`aadhar_no`) REFERENCES `Donor` (`aadhar_no`),
   CONSTRAINT `fk_nurse_id` FOREIGN KEY (`nurse_id`) REFERENCES `Nurse` (`nurse_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=45 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -43,7 +43,7 @@ CREATE TABLE `Donation` (
 
 LOCK TABLES `Donation` WRITE;
 /*!40000 ALTER TABLE `Donation` DISABLE KEYS */;
-INSERT INTO `Donation` VALUES (1,2744356754,'rbc','2020-11-20','200ml',1),(2,735874758373,'Packed Red Blood Cells','2020-12-12','53',NULL),(3,735874758373,'Fresh Frozen Plasma','2020-12-13','6767',1),(4,735874758373,'Packed Red Blood Cells','2020-12-12','53',NULL),(5,735874758374,'Packed Red Blood Cells','2020-12-09','464',1),(6,735874758373,'Fresh Frozen Plasma','2020-12-13','6767',1),(8,1,'Fresh Frozen Plasma','2020-12-19','433',1),(9,2744356754,'Packed Red Blood Cells','2020-12-09','543454',1),(10,2744356754,'Packed Red Blood Cells','2020-12-19','45454',1),(11,2744356754,'Packed Red Blood Cells','2020-12-09','543454',1),(12,2744356754,'Packed Red Blood Cells','2020-12-19','45454',1),(14,1,'Plasma','2020-12-04','4547',1),(16,1,'Plasma','2020-12-04','4547',1),(18,1,'Packed Red Blood Cells','2020-12-13','200',1);
+INSERT INTO `Donation` VALUES (40,287453872434,'Plasma','2020-12-04','200',5),(41,287453872434,'Packed Red Blood Cells','2020-12-11','200',5),(44,433545545454,'Fresh Frozen Plasma','2020-12-03','200',5);
 /*!40000 ALTER TABLE `Donation` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!50003 SET @saved_cs_client      = @@character_set_client */ ;
@@ -74,6 +74,23 @@ DELIMITER ;
 /*!50003 SET character_set_client  = @saved_cs_client */ ;
 /*!50003 SET character_set_results = @saved_cs_results */ ;
 /*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`nishant`@`localhost`*/ /*!50003 TRIGGER `addDonationDate` AFTER INSERT ON `Donation` FOR EACH ROW begin
+	update Donor set donationDate=new.d_date where aadhar_no= new.aadhar_no;
+end */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 
 --
 -- Table structure for table `Donor`
@@ -87,12 +104,13 @@ CREATE TABLE `Donor` (
   `name` varchar(20) DEFAULT NULL,
   `email` varchar(30) DEFAULT NULL,
   `district` varchar(30) DEFAULT NULL,
-  `blood_grp` varchar(5) DEFAULT NULL,
+  `blood_grp` varchar(7) DEFAULT NULL,
   `mob_no` bigint DEFAULT NULL,
   `gender` char(10) DEFAULT NULL,
   `address` varchar(150) DEFAULT NULL,
   `pinCode` int DEFAULT NULL,
   `isPlasmaDonor` varchar(5) DEFAULT NULL,
+  `donationDate` date DEFAULT ((curdate() - interval 6 month)),
   PRIMARY KEY (`aadhar_no`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -103,7 +121,7 @@ CREATE TABLE `Donor` (
 
 LOCK TABLES `Donor` WRITE;
 /*!40000 ALTER TABLE `Donor` DISABLE KEYS */;
-INSERT INTO `Donor` VALUES (1,'Nishant Pandey','riyajoshi@gmail.com',' Mumbai Suburban','A_pos',8369247772,'Female','Roon No. 10, Vikram Chawl, Mulgaon Dongari, Andheri (East),Mumbai-400093,Maharashtra,India',400093,'Yes'),(2744356754,'Rashmi','rashmi123@gmail.com','Aurangabad','O+',8765123337,'female',NULL,NULL,NULL),(735874758373,'Rasika Patil','r@gmail.com','Mumbai City','B+',8369247772,'Female','Andher',400093,'No'),(735874758374,'Nishant Pandey','a@gmail.com','  Bhandara','B-',8369247772,'Male','Roon No. 10, Vikram Chawl, Mulgaon Dongari, Andheri (East),Mumbai-400093,Maharashtra,India',400094,'Yes');
+INSERT INTO `Donor` VALUES (287453872433,'Nishant Pandey','nishantp1821@gmail.com',' Mumbai Suburban','O_pos',8369247772,'Female','Roon No. 10, Vikram Chawl, Mulgaon Dongari, Andheri (East),Mumbai-400093,Maharashtra,India',400093,'Yes','2020-06-03'),(287453872434,'Dipti Pandey','gpmlearningsystem@gmail.com','Jalna','AB_pos',8369247772,'Female','Roon No. 10, Vikram Chawl, Mulgaon Dongari, Andheri (East),Jalana\n-400093,Maharashtra,India',400093,'Yes','2020-12-11'),(433545545454,'Manorama Patil','riyajoshi@gmail.com','Thane','AB_pos',8576643434,'Female','Roon No. 10, Vikram Chawl, Mulgaon Dongari, Andheri (East),Mumbai-400093,Maharashtra,India',400093,'Yes','2020-12-03');
 /*!40000 ALTER TABLE `Donor` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -148,7 +166,7 @@ CREATE TABLE `Hospital` (
   `pincode` int DEFAULT NULL,
   `district` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`h_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -157,7 +175,7 @@ CREATE TABLE `Hospital` (
 
 LOCK TABLES `Hospital` WRITE;
 /*!40000 ALTER TABLE `Hospital` DISABLE KEYS */;
-INSERT INTO `Hospital` VALUES (1,'Jeevan',7845127890,NULL,NULL,NULL),(2,'Jeevan Hospital',9877676767,'Mumbai',8746476,'Jalna'),(4,'Nishant Pandey',9877676767,'Pune',343454,'Ahmednagar');
+INSERT INTO `Hospital` VALUES (7,'Jeevan Hospital',8776767675,'Mumbai',400093,'Mumbai City'),(8,'City Hospital',9877676767,'Kolhapur',333000,'Kolhapur');
 /*!40000 ALTER TABLE `Hospital` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -177,7 +195,7 @@ CREATE TABLE `Nurse` (
   `email` varchar(20) DEFAULT NULL,
   `join_date` date DEFAULT NULL,
   PRIMARY KEY (`nurse_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -186,7 +204,7 @@ CREATE TABLE `Nurse` (
 
 LOCK TABLES `Nurse` WRITE;
 /*!40000 ALTER TABLE `Nurse` DISABLE KEYS */;
-INSERT INTO `Nurse` VALUES (1,'Riyas','female','Pune',6709123348,'riyajoshi@gmail','2020-11-20');
+INSERT INTO `Nurse` VALUES (5,'Riya Joshi','Female','Mumbai',87734872343,'riyajoshi@gmail.com','2020-01-15'),(6,'Manorama Patil','Female','Mumbai',7878787878,'manoram@gmail.com','2018-02-04');
 /*!40000 ALTER TABLE `Nurse` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -208,7 +226,7 @@ CREATE TABLE `Transfer` (
   PRIMARY KEY (`t_id`),
   KEY `fk_h_id` (`h_id`),
   CONSTRAINT `fk_h_id` FOREIGN KEY (`h_id`) REFERENCES `Hospital` (`h_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -217,7 +235,7 @@ CREATE TABLE `Transfer` (
 
 LOCK TABLES `Transfer` WRITE;
 /*!40000 ALTER TABLE `Transfer` DISABLE KEYS */;
-INSERT INTO `Transfer` VALUES (1,1,'2020-11-20','rbc','200ml','o+','pass'),(2,1,'2020-11-24','Fresh Frozen Plasma','200','A-','PASS'),(3,1,'2020-11-24','Fresh Frozen Plasma','200','A-','PASS'),(4,1,'2020-11-19','Packed Red Blood Cells','200','A-','PENDING'),(5,2,'2020-12-13','Packed Red Blood Cells','200','A_pos','PENDING'),(6,2,'2020-12-13','Packed Red Blood Cells','200','A_pos','PENDING'),(7,2,'2020-12-13','Packed Red Blood Cells','200','A_pos','PENDING'),(8,2,'2020-12-13','Packed Red Blood Cells','200','A_pos','PENDING'),(9,2,'2020-12-13','Packed Red Blood Cells','200','A_pos','PENDING');
+INSERT INTO `Transfer` VALUES (10,7,'2020-12-02','Fresh Frozen Plasma','200','B_pos','PENDING');
 /*!40000 ALTER TABLE `Transfer` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!50003 SET @saved_cs_client      = @@character_set_client */ ;
@@ -264,7 +282,7 @@ CREATE TABLE `campaigns` (
   `createdon` bigint DEFAULT NULL,
   `c_date` date DEFAULT NULL,
   PRIMARY KEY (`camp_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -273,7 +291,7 @@ CREATE TABLE `campaigns` (
 
 LOCK TABLES `campaigns` WRITE;
 /*!40000 ALTER TABLE `campaigns` DISABLE KEYS */;
-INSERT INTO `campaigns` VALUES (1,'Bloodrive','Andheri',37465734,38746873,'2020-11-21'),(2,'Dipti Pandey','Andheri',87458734,4785,'2020-12-05'),(3,'Dipti Pandey','Andher',34537,46465454,'2020-12-19'),(4,'Dipti Pande','Andheri',87458734,47857,'2020-12-05');
+INSERT INTO `campaigns` VALUES (6,'Bloodrive','Andheri',7346354533,34343,'2020-12-03'),(7,'Donate Please','Pune',4353454353,243,'2020-12-18');
 /*!40000 ALTER TABLE `campaigns` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -297,7 +315,7 @@ CREATE TABLE `event` (
 
 LOCK TABLES `event` WRITE;
 /*!40000 ALTER TABLE `event` DISABLE KEYS */;
-INSERT INTO `event` VALUES ('Learning Management System',NULL,'#832a2a'),('Introductory email',NULL,'#9d2f2f'),('Introductory email',NULL,'#9d2f2f'),('Learning Management System',NULL,'#c11010'),('Learning Management System','2020-11-03','#a11717'),('Learning Management System','2020-11-03','#a11717'),('Learning Management System','2020-11-03','#a11717'),('hsfjdss','2020-11-05','#bb2a2a'),('Introductory email','2020-11-30','#13b98f'),('Introductory email','2020-11-29','#9b1717'),('Python Running','2020-11-17','#be1919'),('Introductory email','2020-11-11','#9c1c1c'),('Gallery Assignment','2020-12-01','#b42222'),('Introductory email','2020-11-04','#c82828'),('Introductory email','2020-11-06','#9c1c1c'),('Learning Management System','2020-11-04','#d42525'),('Gallery Assignment','2020-11-10','#8f2424'),('Show project','2020-12-03','#cc1414');
+INSERT INTO `event` VALUES ('Introductory email','2020-12-05','#7e1010');
 /*!40000 ALTER TABLE `event` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -327,7 +345,7 @@ CREATE TABLE `storage` (
 
 LOCK TABLES `storage` WRITE;
 /*!40000 ALTER TABLE `storage` DISABLE KEYS */;
-INSERT INTO `storage` VALUES ('Fresh Frozen Plasma',0,0,0,0,0,0,0,0),('Packed Red Blood Cells',0,0,1177016,0,0,0,0,0),('Plasma',0,0,9094,0,0,0,0,0),('Platelet Concentrate',0,0,0,0,0,0,0,0),('Whole Blood',0,0,0,0,0,0,0,0);
+INSERT INTO `storage` VALUES ('Fresh Frozen Plasma',0,0,0,0,0,0,200,0),('Packed Red Blood Cells',0,0,0,0,0,0,2300,0),('Plasma',0,0,0,0,0,0,800,0),('Platelet Concentrate',0,0,0,0,0,0,400,0),('Whole Blood',0,0,0,0,0,0,187,0);
 /*!40000 ALTER TABLE `storage` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -340,4 +358,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-12-02 11:42:09
+-- Dump completed on 2020-12-09 10:37:55
